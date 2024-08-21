794a63620a3a15b106f49819c515e7ab9a49119c
Ryan Lee <ryanleeraniaga@gmail.com>
2024-08-21 09:04:44 +0800

    add envs

--- app/(pages)/(website)/(home)/page.tsx
+++ app/(pages)/(website)/(home)/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <main>
+   <main className="">
     <h1>Home</h1>
+
+    <Image src="/assets/images/logo.png" width="200" height="200" alt="logo" />
    </main>
   );
 }
